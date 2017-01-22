834f35ad226ccc8e1ad66cc70dd5deb784dabe38
jwegmann27 <josephwe27@gmail.com>
2017-01-22 15:11:11 -0500

    Started Messing with Upper.js

--- src/components/Upper.js
+++ src/components/Upper.js
@@ -3,9 +3,25 @@ import React from 'react';
 
 class Upper extends React.Component {
   render() {
+    const bgImg = require('../css/images/test-bg-upper.jpg');
+
+    const bgImgStyle = {
+      background: 'url(' + bgImg + ')',
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundAttachment: "fixed",
+      backgroundColor: "#e1e1e1",
+      height: "100vh",
+      width: "100%",
+    };
     return (
-      <div>
-        <h1>Upper</h1>
+      <div className="upper-wrap">
+        <div style={bgImgStyle} className="upper-full-image"></div>
+        <div className="upper-content">
+          <h1>They're robots Morty! It's okay to shoot them! They're just robots!</h1>
+          <h1>It's a figure of speech, Morty. They're bureaucrats. I don't respect them. Just keep shooting, Morty. You have no idea what prison is like here!</h1>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 
+import './css/style.css';
+
 import App from './components/App';
 import NotFound from './components/NotFound';
 
